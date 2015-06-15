73eab1e875c32e854813bafef0a6c95b0c146628
unknown <dnotis@CDT-P24120A.cubitus.local>
2015-06-15 17:14:41 -0400

    Premier Commit

---
+++ tp.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+import os
+
+
+class Dictionnaire:
+    """ La premiers class qui va contenir les méthodes ou Attribut ordinnaire d'un dictionnaire.
+        Création, ajout, Suppresion , Modification
+    """
+
+    def __init__(self,nom):
+        if self.nom != type(str):
+            self= {}
+
+        
+
+
+
+
+class DictionnaireOrdonne(Dictionnaire):
+    """ Recoit Dictionnaire comme mère
+    """
+
+
+
+
+os.system("pause")
